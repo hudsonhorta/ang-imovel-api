eb53d61b105edc6d21aebdad627199f41c271213
Hudson Horta <hudsonhorta@gmail.com>
2022-10-18 10:59:55 -0400

    cors

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 3000;
 const cidades = require("./dados/cidades.json");
 
 app.get("/cidades", (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
     return res.json(cidades);
 })
 
