c5b9997f9818e053b854e609161efdcb9d3bdc87
Hudson Horta <hudsonhorta@gmail.com>
2022-10-18 17:29:33 -0400

    cors

--- index.js
+++ index.js
@@ -3,24 +3,19 @@ const cors = require("cors");
 const port = process.env.PORT || 3000;
 const app = express();
 
-require('./src/Routes/index')(app)
-
 app.use(express.json());
 
 app.use(cors({
     origin: 'https://ang-imovel.herokuapp.com'
 }))
 
-//const bairros = require("./dados/bairros.json");
+// rotas
 
-// app.get("/cidades", (req, res) => {
-//     res.json(cidades);
-// })
+const cidades = require('./src/Routes/CidadesRoute')
+const bairros = require('./src/Routes/BairrosRoute')
 
-// app.get("/bairros", (req, res) => {
-//     res.json(bairros[9]);
-// })
-
+app.use('/', cidades)
+app.use('/', bairros)
 
 app.listen(port, () => {
     console.log("Servidor rodando");
